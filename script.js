685f1768359f4d3cf60a86bbea8a9734221b3663
Michael Lopez <mlopez@rightslne.com>
2021-03-26 12:06:36 -0700

    Color generator

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 // generator function
-
+function getColor() {
+    return '#' + Math.random().toString(16).slice(2, 8);
+  }
 
 
 // sets background color style
-
+function setBackground() {
+    let bgColor = getColor(); 
+    document.body.style.background = bgColor; 
+}
 
 
 //pic generator function
-
+function getPhoto(){
+    let randomPhoto =  document.querySelector('#photo');
+     randomPhoto.src = "https://picsum.photos/500?random=1";
+     randomPhoto.style.display
+   }
 
 
 // runs function on click
- 
- 
+document.body.onkeyup = function(e) {
+    if(e.keyCode == 32){
+        getPhoto();
+        setBackground();
+    }
+}
 
